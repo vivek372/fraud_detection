61185e0c49231711b5d67521e9352957a1963bd0
vivek372 <vivek372@gmail.com>
2023-07-08 13:58:41 +0530

    added 3 new model codes

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("Adding all the ML related code here")
+
+def model1():
+    print("this is model 1")
+
+def model2():
+    print("this is model 2")
+
+def model3():
+    print("this is model 3")
